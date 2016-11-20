06b9bb06782f3c7ea928c45baee171ddb8f2ee70
liyatang <liyatang@xiaonongnv.com>
2016-11-21 00:00:35 +0800

    优化

--- src/index.js
+++ src/index.js
@@ -1,39 +1,95 @@
-const md = require('markdown-it')();
 const mdContainer = require('markdown-it-container');
+const RegexPlugin = require('markdown-it-regexp');
 const frontMatter = require('front-matter');
+var highlight = require('highlight.js');
+let md = require('markdown-it');
 
-const formatModule = (imports, content) => {
+md = md('commonmark').use(
+    // TODO copy
+    // Plugin to pass through assignment expressions without escaping
+    RegexPlugin(/{({\s*(?:<.*?>|.*?)\s*})}/, (match) => match[1])
+).set({
+    // TODO copy
+    highlight(content, languageHint){
+        // const preprocessed_content = {};
+        let highlightedContent;
+
+        highlight.configure({
+            useBR: true,
+            tabReplace: '  '
+        });
+
+        // Hold onto JSX assignment expressions before passing to highlighter
+        // content = content.replace(/{({\s*(?:<.*?>|.*?)\s*})}/g, (match, value) => {
+        //     const key = hash('sha256').update(value, 'utf-8').digest('hex');
+        //     preprocessed_content[key] = value;
+        //     return key;
+        // });
+
+        // Try highlighting with a given hint
+        if (languageHint && highlight.getLanguage(languageHint)) {
+            try {
+                highlightedContent = highlight.highlight(languageHint, content).value;
+            } catch (err) {
+            } // eslint-disable-line no-empty
+        }
+
+        // Highlight without a hint
+        if (!highlightedContent) {
+            try {
+                highlightedContent = highlight.highlightAuto(content).value;
+            } catch (err) {
+            } // eslint-disable-line no-empty
+        }
+
+        // Quote curly braces
+        highlightedContent = highlightedContent.replace(/[\{\}]/g, (match) => `{'${match}'}`);
+
+        // Put back the JSX assignment expressions we pulled out before returning
+        // Object.keys(preprocessed_content).forEach((key) =>
+        //     highlightedContent = highlightedContent.replace(key, preprocessed_content[key])
+        // );
+
+        return highlight.fixMarkup(highlightedContent);
+    }
+});
+
+const formatModule = (imports, js, jsx) => {
     let moduleText = `
     ${imports}
     
-    function MarkdownComponent(props) {
-      return (
-        <div>
-          ${content}
-        </div>
-      );
+    ${js}
+    
+    class MarkdownItReactComponent extends React.Component {
+        render(){
+            return (
+                <div>
+                    ${jsx}
+                </div>
+            );
+        }
     };
 
-    export default MarkdownComponent;`;
+    export default MarkdownItReactComponent;`;
 
     return moduleText;
 };
 
-const formatCode = (code) => {
+const formatOpening = (code, description) => {
     return (
         `<div className="demo-box">
     <div className="demo-instance">
         <h4>Example</h4>
-        {${code}}
+        ${code}
     </div>
     <div className="demo-meta">
+        <div className="demo-description">${description}</div>
         <div className="demo-code">`);
 };
 
-const formatDescription = (description) => {
+const formatClosing = () => {
     return (
-        `        </div>
-        <div className="demo-description">${description}</div>
+        `</div>
     </div>
 </div>`);
 };
@@ -44,27 +100,50 @@ module.exports = function (source) {
     const {body, attributes: {imports: importMap}} = frontMatter(source);
     const imports = 'import React from \'react\'; ' + importMap;
 
-    let description = '';
+    let moduleJS = '';
 
     md.use(mdContainer, 'demo', {
         validate: function (params) {
             return params.trim().match(/^demo\s+(.*)$/);
         },
         render: function (tokens, idx) {
+            // container 从开头到结尾把之间的token跑一遍，其中idx定位到具体的位置
+
+            // 获取描述
             const m = tokens[idx].info.trim().match(/^demo\s+(.*)$/);
 
+            // 有此标记代表 ::: 开始
             if (tokens[idx].nesting === 1) {
-                const code = tokens[idx + 1].content;
-                description = md.utils.escapeHtml(m[1]);
+                let jsx = '', i = 1;
 
-                return formatCode(code);
+                // 从 ::: 下一个token开始
+                let token = tokens[idx + i];
+
+                // 如果没有到结尾
+                while (token.markup !== ':::') {
+                    // 只认```，其他忽略
+                    if (token.markup === '```') {
+                        if (token.info === 'js') {
+                            // 插入到import后，component前
+                            moduleJS = token.content;
+                        } else if (token.info === 'jsx') {
+                            // 插入render内
+                            jsx = token.content;
+                        }
+                    }
+                    i++;
+                    token = tokens[idx + i]
+                }
+
+                // 描述也执行md
+                return formatOpening(jsx, md.render(m[1]));
             }
-            return formatDescription(description);
+            return formatClosing();
         }
     });
 
     // md 处理过后的字符串含有 class 和 style ，需要再次处理给到react
     const content = md.render(body).replace(/class=/g, 'className=').replace(/<hr>/g, '<hr />').replace(/<br>/g, '<br />');
 
-    return formatModule(imports, content);
+    return formatModule(imports, moduleJS, content);
 };
